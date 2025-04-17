99131bb9dea90a1d1af9ad843902b7fae3e9db51
lavraicevic <raiceviclav2107@gmail.com>
2025-04-17 19:45:40 +0200

    Fix the bugs
    Problem on the front-end

--- controllers/API/posts.php
+++ controllers/API/posts.php
@@ -3,6 +3,14 @@
 use Core\App;
 use Core\Database;
 
+header('Access-Control-Allow-Origin: http://127.0.0.1:5500');
+
+// Specify which request methods are allowed
+header('Access-Control-Allow-Methods: PUT, GET, POST, DELETE, OPTIONS');
+
+// Additional headers which may be sent along with the CORS request
+header('Access-Control-Allow-Headers: X-Requested-With,Authorization,Content-Type');
+
 $db = App::resolve(Database::class);
 
 $posts = $db->query('SELECT * FROM POSTS');
@@ -10,3 +18,5 @@ $posts = $db->query('SELECT * FROM POSTS');
 $data = $posts->get();
 
 echo json_encode($data);
+
+exit;
